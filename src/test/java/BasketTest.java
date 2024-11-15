38dbcbc467679c4657f3f7c86ddf089b55b6c4a6
Khaled Mohsen <khaledmohsenmohammed@gmail.com>
2024-11-16 00:30:04 +0200

    first commit

--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,7 +1,8 @@
+// Import Allure annotations for reporting test metadata
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
-import io.qameta.allure.Severity;
 import io.qameta.allure.Story;
+// Import Selenium classes for browser interaction
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,24 +10,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+// Import TestNG for managing test lifecycle and assertions
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+// Import Java time package for timeout durations
 import java.time.Duration;
 
+// Class definition for testing the basket functionality
 public class BasketTest {
+    // Define WebDriver instance to interact with the browser
     private WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        // Initialize the ChromeDriver
+        // Set the ChromeDriver executable path for browser automation
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
         driver = new ChromeDriver();
-        // Open the browser in full screen
+        // Maximize the browser window for better visibility
         driver.manage().window().maximize();
-        // Open Vodafone Czech Republic website
+        // Open the Vodafone Czech Republic website
         driver.get("https://www.vodafone.cz/");
     }
     @Test
@@ -34,31 +38,32 @@ public class BasketTest {
     @Feature("Shopping Cart")
     @Story("User should be able to add an iPhone to the cart.")
     public void addToBasket() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Define explicit wait
+        // Initialize explicit wait with a timeout of 10 seconds
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Wait for cookies popup to be visible and click accept
+        // Wait for the cookies pop-up to appear and click the accept button
         WebElement cookiesPopUp = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//*[@id='homepage']/div[8]/div/p[2]/button[1]")));
         cookiesPopUp.click();
 
-        // Wait for "Obchod" element to be visible and hover over it
+        // Wait for the "Obchod" (Shop) menu item to appear and hover over it
         WebElement obchod = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Obchod']")));
-        System.out.println("********************************* " + obchod.getText());
+        System.out.println("*********************************>> " + obchod.getText());
         Actions actions = new Actions(driver);
         actions.moveToElement(obchod).perform();
 
-        // Wait for the iPhone menu item to be clickable and click it
+        // Wait for the iPhone menu item to become clickable and click it
         WebElement iphone = wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//*[@id='vf-header']/div[3]/div/div[1]/div[2]/div/nav/ul/li[4]/ul/li[2]/ul/li[1]/a")));
-        System.out.println("********************************* " + iphone.getText());
+        System.out.println("*********************************>> " + iphone.getText());
         actions.click(iphone).perform();
 
-        // Wait for the first iPhone link to be visible and click it
+        // Wait for the first iPhone product link to appear and click it
         WebElement firstIphone = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//*[@id='product_apple-iphone-16-128-gb:iphone16128b']/div/div[2]/div/p[1]/a")));
         firstIphone.click();
 
-        // Assert that the title contains "iPhone"
+        // Validate that the page title contains "iPhone"
         try {
             String pageTitle = driver.getTitle();
             Assert.assertTrue(pageTitle.contains("iPhone"), "The page title does not contain 'iPhone'.");
